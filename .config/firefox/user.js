464ff87dfac2ceb74d39d566580de2a0883d5c69
SystemCirno64 <60094299+slimulv1@users.noreply.github.com>
2026-08-29 15:25:20 +0700

    Firefox: transparent chrome theme (userChrome.css + user.js allowTransparentBrowser)

---
+++ .config/firefox/user.js
@@ -0,0 +1,3 @@
+// user.js - applied at Firefox startup
+// Cho phép vẽ cửa sổ nền trong suốt (cần cho giao diện transparent qua userChrome.css)
+user_pref("browser.tabs.allowTransparentBrowser", true);
